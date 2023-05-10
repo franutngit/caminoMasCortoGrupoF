c8535adec69231d3ade871b72687e754c99af738
agubir87 <luis.biritos@alumnos.frm.utn.edu.ar>
2023-05-10 15:05:18 -0300

    Ajustes de modularizacion

--- prueba dijkstra/v5/main.py
+++ prueba dijkstra/v5/main.py
@@ -1,31 +1,29 @@
-"""Prueba Dijkstra - Quinto ajuste: Clases para datos y usuario"""
+"""Prueba Dijkstra - Sexto ajuste: Modularizacion"""
 
 from grafo import Grafo
 from datos import Datos as dat
 from usuario import Usuario
 
+"""Datos: Vertices y Aristas"""
+v = dat.vertices()
+a = dat.aristas(v)
 
-class main:
-    """Datos: Vertices y Aristas"""
-    v = dat.vertices()
-    a = dat.aristas(v)
+'''Armado del grafo'''
+g = Grafo()
+for e in v:
+    g.agregarVertice(e)
+for e in a:
+    g.agregarArista(e[0], e[1], e[2])
 
-    '''Armado del grafo'''
-    g = Grafo()
-    for e in v:
-        g.agregarVertice(e)
-    for e in a:
-        g.agregarArista(e[0], e[1], e[2])
+'''Interaccion con el usuario'''
 
-    '''Interaccion con el usuario'''
+o = Usuario().origen(v)
+print(f'ORIGEN escogido: {o}\n')
+d = Usuario().destino(v)
+print(f'DESTINO escogido: {d}')
 
-    o = Usuario().origen(v)
-    print(f'ORIGEN escogido: {o}\n')
-    d = Usuario().destino(v)
-    print(f'DESTINO escogido: {d}')
-
-    '''Camino mas corto mediante Dijkstra'''
-    # Se aplica el algoritmo con el vertice inicial
-    g.dijkstra(o)
-    # Se muestra el camino mas corto con los vertices inicial y final
-    print(g.camino(o, d))
+'''Camino mas corto mediante Dijkstra'''
+# Se aplica el algoritmo con el vertice inicial
+g.dijkstra(o)
+# Se muestra el camino mas corto con los vertices inicial y final
+print(g.camino(o, d))
