75901a01b63b8e166c0d2c68cac880d28bddb63b
agubir87 <luis.biritos@alumnos.frm.utn.edu.ar>
2023-05-10 14:43:23 -0300

    Ajustes de modularizacion

--- prueba dijkstra/v5/grafo.py
+++ prueba dijkstra/v5/grafo.py
@@ -1,5 +1,21 @@
+"""Clase que define los vertices del grafo"""
+
+
+class Vertice:
+    def __init__(self, i):
+        self.id = i
+        self.vecinos = []
+        self.visitado = False
+        self.predecesor = None
+        self.distancia = float('inf')
+
+    def agregarVecino(self, v, p):
+        # el condicional es para evitar la repeticion de vertices
+        if not v in self.vecinos:
+            self.vecinos.append([v, p])
+
+
 """Clase que define el grafo y calcula el camino mas corto"""
-from vertice import Vertice
 
 
 class Grafo:
