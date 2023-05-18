54ddae85b1edb0e9ae9c83e575c0904c899f2418
franc <francosm@jarvistec.com>
2023-05-18 18:05:58 -0300

    commit funciona!

--- InterfazGrafica.py
+++ InterfazGrafica.py
@@ -1,64 +1,37 @@
 import folium
+import osmnx as ox
 import networkx as nx
-from geopy.distance import geodesic
-
-def shortest_path(start, end, waypoints):
-    # Crear el grafo
-    G = nx.Graph()
-
-    # Agregar los puntos de inicio y fin al grafo
-    G.add_node('Start', pos=start)
-    G.add_node('End', pos=end)
-
-    # Agregar los puntos intermedios al grafo
-    for i, waypoint in enumerate(waypoints):
-        G.add_node(f'Waypoint{i}', pos=waypoint)
-
-    # Calcular las distancias entre los puntos
-    nodes = list(G.nodes())
-    for i in range(len(nodes)):
-        for j in range(i + 1, len(nodes)):
-            node1, node2 = nodes[i], nodes[j]
-            dist = geodesic(G.nodes[node1]['pos'], G.nodes[node2]['pos']).meters
-            G.add_edge(node1, node2, weight=dist)
-
-    # Encontrar el camino más corto utilizando el algoritmo Dijkstra
-    path = nx.dijkstra_path(G, 'Start', 'End', weight='weight')
-
-    return path
 
 # Coordenadas de ejemplo
-start_coord = (-32.897330, -68.853555)  # Nueva York
-end_coord = (-32.904422, -68.842819)  # Los Ángeles
-waypoints_coords = []  # Chicago y Filadelfia
+start_coord = (-32.897330, -68.853555)  # Utn
+end_coord = (-32.904422, -68.842819)  # Casino de Mendoza
 
-# Encontrar el camino más corto
-path_coords = shortest_path(start_coord, end_coord, waypoints_coords)
+# Obtener el grafo de la red de calles en un área cercana al punto de inicio para conducción en auto
+graph = ox.graph_from_point(start_coord, dist=3000, network_type='drive')
+
+# Convertir el grafo a un grafo no dirigido para encontrar la ruta más corta
+graph = graph.to_undirected()
+
+# Agregar los nodos de inicio y fin al grafo
+start_node = ox.distance.nearest_nodes(graph, start_coord[1], start_coord[0])
+end_node = ox.distance.nearest_nodes(graph, end_coord[1], end_coord[0])
+graph.add_nodes_from([(start_node, {'pos': start_coord}), (end_node, {'pos': end_coord})])
+
+# Encontrar el camino más corto utilizando el algoritmo Dijkstra
+path = nx.shortest_path(graph, start_node, end_node, weight='length')
+
+# Obtener las coordenadas de los nodos en el camino más corto
+path_coords = [(graph.nodes[node]['y'], graph.nodes[node]['x']) for node in path]
 
 # Crear el mapa
-map = folium.Map(location=start_coord, zoom_start=4)
+map = folium.Map(location=start_coord, zoom_start=14)
 
 # Agregar marcadores al mapa
-for i, coord in enumerate(path_coords):
-    if coord == 'Start':
-        folium.Marker(start_coord, popup='Start').add_to(map)
-    elif coord == 'End':
-        folium.Marker(end_coord, popup='End').add_to(map)
-    else:
-        folium.Marker(coord, popup=f'Waypoint {i}').add_to(map)
+folium.Marker(start_coord, popup='Start').add_to(map)
+folium.Marker(end_coord, popup='End').add_to(map)
 
 # Agregar la línea que representa el camino más corto al mapa
-polyline_coords = []
-for coord in path_coords:
-    if coord == 'Start':
-        polyline_coords.append(start_coord)
-    elif coord == 'End':
-        polyline_coords.append(end_coord)
-    else:
-        polyline_coords.append(coord)
-
-folium.PolyLine(polyline_coords, color='blue', weight=2.5, opacity=1).add_to(map)
+folium.PolyLine(path_coords, color='blue', weight=2.5, opacity=1).add_to(map)
 
 # Mostrar el mapa
-map
 map.save("map.html")
